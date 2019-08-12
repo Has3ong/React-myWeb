25c4580d1283ed157bbcbea3d1440a9f77e71ac1
Has3ong <khsh5592@naver.com>
2019-08-13 02:17:57 +0900

    update react mybook

---
+++ mybook/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'Javascript: The Good Parts' },
+        { title: 'Harry Poter' },
+        { title: 'Eloquent Ruby' },
+        { title: 'The game of the Throne'}
+    ]
+}
