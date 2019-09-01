d2bf81b211e75cf6ed4eb8c9f877428f3d84c020
has3ong <khsh5592@naver.com>
2019-09-01 22:06:03 +0900

    2019-09-01

--- mybook/src/reducers/reducer_books.js
+++
@@ -1,8 +0,0 @@
-export default function() {
-    return [
-        { title: 'Javascript: The Good Parts' },
-        { title: 'Harry Poter' },
-        { title: 'Eloquent Ruby' },
-        { title: 'The game of the Throne'}
-    ]
-}
